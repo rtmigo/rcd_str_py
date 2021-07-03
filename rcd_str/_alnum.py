2ce9138f1c762425a6edc457903316bfbc51ec4e
rtmigo <art.galkin@gmail.com>
2021-07-03 09:18:26 +0300

    _alnum

---
+++ rcd_str/_alnum.py
@@ -0,0 +1,53 @@
+import re
+from typing import List
+
+
+def splitWaN(text):
+    return alphanumerics(text, underscore=False, separate_numbers=True)
+
+
+def alphanumerics(text,
+                  underscore=False,
+                  separate_numbers=False):
+    # todo test
+
+    # \w		буква, цифра или "_"
+    # \W		всё, кроме "\w". То есть, не буква, не цифра и не подчеркивание
+    # [^\W]		отрицание \W экваивалентно отрицанию отрицания \w,
+    #           то есть эквивалентно \w
+    # [^\W_]	"\w", но не "_"
+
+    if separate_numbers:
+        if underscore:
+            return re.findall(r"[^\W\d]+|\d+", text)
+        else:
+            return re.findall(r"[^\W\d_]+|\d+", text)
+    else:
+        if underscore:
+            return re.findall(r"\w+", text)  # ?! не тестировано!
+        else:
+            return re.findall(r"[^\W_]+", text)  # ?! не тестировано!
+
+
+def alphawords(text: str, underscore: bool = False) -> List[str]:
+    """Returns all alpha character sequences from the `text`.
+
+    If `underscore` is True, the '_' is also treated as an alpha character.
+    """
+    if underscore:
+        return re.findall(r"[^\W\d]+", text)
+    else:
+        return re.findall(r"[^\W\d_]+", text)
+
+
+def keepalnum(txt: str) -> str:
+    # http://stackoverflow.com/questions/1276764/
+    # stripping-everything-but-alphanumeric-chars-from-a-string-in-python
+    pattern = re.compile(r'[\W_]+')
+    return pattern.sub('', txt)
+
+
+def simpleWaN(text):
+    """для приблизительного сравнения строк. Удаляет пунктуацию, минимизирует
+    пробелы, переводит строку в верхний регистр."""
+    return " ".join(splitWaN(text.upper()))
