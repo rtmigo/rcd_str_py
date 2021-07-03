2d68bba9f3dc65f5f9152da284e4e970d07c1101
rtmigo <art.galkin@gmail.com>
2021-07-03 07:10:22 +0300

    ci

--- lint.py
+++ lint.py
@@ -6,12 +6,12 @@ def lint():
     # pip3 install --upgrade setuptools pip distlib
 
     print("Running pylint...")
-    r = subprocess.call(['pylint', 'rcd_wikipedia'])
+    r = subprocess.call(['pylint', 'rcd_str'])
     if r & 1 or r & 2 or r & 32:
         exit(1)
 
     print("Running mypy...")
-    if subprocess.call(['mypy', 'rcd_wikipedia',
+    if subprocess.call(['mypy', 'rcd_str',
                         '--ignore-missing-imports']) != 0:
         exit(1)
 
